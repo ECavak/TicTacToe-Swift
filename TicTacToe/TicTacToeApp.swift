9e7c9eaac8fbaaedb9477d1ea1856c872f4fd926
ECavak <emrecavk@gmail.com>
2022-11-12 16:19:58 +0300

    first commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Emre Cavak on 12.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
